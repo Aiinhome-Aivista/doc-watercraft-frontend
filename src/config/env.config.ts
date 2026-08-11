8dfe007e7bf6c1ca2cff773bf01d4f3b9b876f22
Sonia Khatun <sonia2khatun4@gmail.com>
2026-08-11 13:53:36 +0530

    env host chng to 165.

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -4,7 +4,8 @@
  */
 export const ENV = {
   // Use Vite's `import.meta.env`
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001/api/v1',
+  // API_BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001/api/v1',
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://165.101.183.152:5001/api/v1',
   // API_BASE_URL: import.meta.env.VITE_API_BASE_URL || 'https://aiinhome.com/haldiamultimodalv2/api/v1',
   API_VERSION: import.meta.env.VITE_API_VERSION || 'v1',
   IS_PROD: import.meta.env.MODE === 'production',
