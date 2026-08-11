c22a7adacac9e471f987c77a8de26d01a5973ca1
Sonia Khatun <sonia2khatun4@gmail.com>
2026-08-11 14:08:11 +0530

    main url hit slash issue

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,32 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // plugins: [
+  //   react(),
+  //   tailwindcss(),
+  //   babel({ presets: [reactCompilerPreset()] })
+  // ],
   plugins: [
     react(),
     tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+
+    {
+      name: 'redirect-base-without-slash',
+
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === '/dock_mgmt_v1') {
+            res.statusCode = 302
+            res.setHeader('Location', '/dock_mgmt_v1/')
+            res.end()
+            return
+          }
+
+          next()
+        })
+      }
+    }
   ],
   base: '/dock_mgmt_v1/',
   resolve: {
